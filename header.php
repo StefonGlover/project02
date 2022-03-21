d07fc70f2a149445f51c108bde9549f70a0cc5f2
Stefon <52985099+StefonGlover@users.noreply.github.com>
2022-03-20 20:31:09 -0400

    Added navbar

--- header.php
+++ header.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="jeopardy.css" type="text/css" rel="stylesheet">
-    <title>Project 2</title>
+    <link href="main.css" rel="stylesheet">
+    <title>JEOPARDY!</title>
 </head>
 <body>
-    <div id="banner">
-        <p id="banner-text">JEOPARDY!</p>
+
+<nav>
+    <div class="logo">
+        <p>Jeopardy!</p>
     </div>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</nav>
